576a276ac70c733c3929f235a618bbb9856980ce
Robert Bishop <rbishop85@gmail.com>
2022-09-18 22:21:26 -0700

    added function to homepage.js to capitalize the first letter of the displayed username

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,3 +1,13 @@
+let capitalizeUsername = "";
+
+const capitalizeFirstLetter = () => {
+  capitalizeUsername = username.charAt(0).toUpperCase() + username.slice(1);
+}
+
+capitalizeFirstLetter();
+
+document.getElementById('welcome-message').innerHTML = ("Welcome, " + capitalizeUsername + "!");
+
 const delButtonHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
